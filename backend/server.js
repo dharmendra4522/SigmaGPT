1cdba2c200cbb799a21c2c93e3ca3e054b2dba04
dharmendra4522 <dharmendra4522@gmail.com>
2025-10-03 11:11:17 +0530

    fix: update CORS for production frontend URL

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://sigmagpt.vercel.app',
     credentials: true,
 }));
 app.use(cookieParser());
